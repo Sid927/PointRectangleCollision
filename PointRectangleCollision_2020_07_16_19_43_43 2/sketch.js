1435893a27a1fbed16ae497c78c9593bb3c30266
Sid927 <54078962+Sid927@users.noreply.github.com>
2020-08-24 12:10:35 -0700

    Update sketch.js
    Changed window width and height

--- PointRectangleCollision_2020_07_16_19_43_43 2/sketch.js
+++ PointRectangleCollision_2020_07_16_19_43_43 2/sketch.js
@@ -1,8 +1,10 @@
-let rx1 = 300;
-let ry1 = 400;
 let rx2 = 0;
 let ry2 = 0;
 let hit = false;
+let changableWidth = 500;
+let height = changableWidth / 2;
+let rx1 = window.innerWidth / 2 - 1 / 2 * changableWidth;
+let ry1 = window.innerHeight / 2 - 1 / 2 * height;
 
 const checkCollisionPointRectangle = (px, py, rx1, ry1, rx2, ry2) => {
   if (px >= rx1 && px <= rx2 && py >= ry1 && py <= ry2) {
@@ -13,16 +15,16 @@ const checkCollisionPointRectangle = (px, py, rx1, ry1, rx2, ry2) => {
 }
 
 function setup() {
-  createCanvas(750, 750);
+  createCanvas(window.innerWidth, window.innerHeight);
 }
 
 function draw() {
   hit = false;
   background(255);
   point(mouseX, mouseY);
-  rect(rx1, ry1, 300, 150);
-  rx2 = rx1 + 300;
-  ry2 = ry1 + 150;
+  rect(rx1, ry1, changableWidth, height);
+  rx2 = rx1 + changableWidth;
+  ry2 = ry1 + height;
   hit = checkCollisionPointRectangle(mouseX, mouseY, rx1, ry1, rx2, ry2);
   stroke( (hit) ? color("red"): 0);
   console.log('Colliding: ' + hit)
